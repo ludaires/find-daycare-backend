2ac9695fd42a9d812f7fec54a9ede80191596248
Ludmilla Aires Negri <ludaires@gmail.com>
2019-08-16 15:37:49 -0400

    remove helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
-    helpers do
-        def current_user
-            # mocked version of being logged in
-            User.first
-        end
-
-        def logged_in?
-            !!current_user
-        end
+    
+    def current_user
+        # mocked version of being logged in
+        User.first
     end
+
+    def logged_in?
+        !!current_user
+    end
+   
 end
