8955db140df456c953211e12a2ae063bc0804451
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-08 15:37:48 -0400

    add phone attribute and remove reviews.

--- app/serializers/daycare_serializer.rb
+++ app/serializers/daycare_serializer.rb
@@ -1,6 +1,6 @@
 class DaycareSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :location, :rating, :review_count, :reviews, :yelp_id
+  attributes :id, :name, :location, :rating, :review_count, :yelp_id, :phone
   has_many :my_daycares
   has_many :users, through: :my_daycares
 end
