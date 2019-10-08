8018430da83f2241748e92c69dbedd0ce086bd0e
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-08 15:35:27 -0400

    Add create daycares action.

--- app/controllers/api/v1/daycares_controller.rb
+++ app/controllers/api/v1/daycares_controller.rb
@@ -7,12 +7,38 @@ class Api::V1::DaycaresController < ApplicationController
         render json: daycares_json
     end
 
+    def create 
+        daycare = Daycare.find_by(yelp_id: params[:daycare][:yelp_id])
+        if !!daycare
+            if !!current_user.daycares.find_by(yelp_id: params[:daycare][:yelp_id])
+                # binding.pry
+                # daycare = current_user.daycares.find_by(yelp_id: params[:daycare][:yelp_id])
+                # @mydaycare = MyDaycare.find_by(user_id: current_user.id, daycare_id: daycare.id)
+                # my_daycare_json = MyDaycareSerializer.new(@my_daycare).serialized_json
+                render json: {error: "You have already salved this daycare to your list"}
+            else
+                @mydaycare = MyDaycare.create(user_id: current_user.id, daycare_id: daycare.id)
+                my_daycare_json = MyDaycareSerializer.new(@my_daycare).serialized_json
+                render json: my_daycare_json
+            end
+        else
+            daycare = Daycare.new(daycare_params)
+            if daycare.save
+                @mydaycare = MyDaycare.create(user_id: current_user.id, daycare_id: daycare.id)
+                my_daycare_json = MyDaycareSerializer.new(@my_daycare).serialized_json
+                render json: my_daycare_json
+            else
+                render json: daycare.errors, status: :unprocessable_entity
+            end
+        end
+    end
+
     private
     def get_daycare
         @daycare = Daycare.find(params[:id])
     end
 
     def daycare_params
-        params.require(:daycare).permit(:name, :location, :rating, :review_count, :reviews, :yelp_id)
+        params.require(:daycare).permit(:name, :location, :rating, :review_count, :yelp_id, :phone)
     end
 end
