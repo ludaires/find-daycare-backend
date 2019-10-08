969a40e125fc39c7f1388286752693c7c6e4d4ce
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-08 15:37:04 -0400

    add create, update and destroy actions.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,30 @@ class Api::V1::UsersController < ApplicationController
         render json: user_json
     end
     
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id
+            render json: UserSerializer.new(@user), status: :created 
+        else
+            resp = { error: @user.errors.full_messages.to_sentence }
+            render json: resp, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @user.update(user_params)
+            render json: @user
+        else
+            render json: @user.erros, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user.destroy
+    end
+
     private
     def get_user
         @user = User.find(params[:id])
