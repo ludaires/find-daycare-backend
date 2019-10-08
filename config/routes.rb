37414ffff1219a1681b1a574280e96136f60a3bf
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-08 15:39:18 -0400

    add routes to destroy user and signup.

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   post '/api/v1/login', to: "api/v1/sessions#create"
   get '/api/v1/get_current_user', to: "api/v1/sessions#get_current_user"
+  delete '/api/v1/destroy', to: "api/v1/sessions#destroy"
 
+  post '/api/v1/signup', to: "api/v1/users#create"
 
 end
